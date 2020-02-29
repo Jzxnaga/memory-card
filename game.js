41c6665cfc5986fc63f7329ff8ed185ca2977f58
bzxnaga <bzxnaga@gmail.com>
2020-02-29 18:50:43 +0700

    3rd

--- game.js
+++ game.js
@@ -1,8 +1,38 @@
 const cards = document.querySelectorAll('.memory-card');
 
+function flipCard() {
+  this.classList.toggle('flip');
 
-function flipcard(){
-    this.classlist.toggle('flip');
+if(hasFlippedCard=false){
+
+    hasFlippedCard=true
+    FirstCard = this;
+
+}else{
+
+    hasFlippedCard = false
+    seconCard = this;
+
+// console.log(firstCard.dataset.image);
+// console.log(secodCard.dataset.image);
+
+
+if(firstCard.dataset.image === secodCard.dataset.image){
+    firstCard.removeEventListener('click',flipCard);
+    secodCard.removeEventListener('click',flipCard);
+} else {
+    
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
 }
 
-cards.forEach(card = > card.addEventListener('click',flipCard))
+
+
+    }
+}
+
+
+
+
+
+cards.forEach(card => card.addEventListener('click', flipCard));
