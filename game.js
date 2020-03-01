cf267ce6a15cd5744ab0c141dd74cbbe25478bfa
bzxnaga <bzxnaga@gmail.com>
2020-03-01 07:34:49 +0700

    test asik

--- game.js
+++ game.js
@@ -1,44 +1,84 @@
+
 const cards = document.querySelectorAll('.memory-card');
+const reset = document.querySelectorAll('.reset-card');
+
 
 let hasFlippedCard = false;
+let lockCard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+  if (lockCard) return;
+  if (this === firstCard) return;
+
   this.classList.add('flip');
 
   if (!hasFlippedCard) {
-    
+    // first click
     hasFlippedCard = true;
     firstCard = this;
 
     return;
-
   }
 
-  hasFlippedCard = false;
+  // second click
   secondCard = this;
 
-  MatchOrNo();
+  checkForMatch();
 }
-// console.log(firstCard.dataset.image);
-// console.log(secodCard.dataset.image);
 
-function MatchOrNo() {
-    let isMatch = firstCard.dataset.image === secondCard.dataset.image;
+function checkForMatch() {
+  let isMatch = firstCard.dataset.image === secondCard.dataset.image;
+
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
+}
+
+function unflipCards() {
+  lockCard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 800);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockCard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+// (function shuffle() {
+//   cards.forEach(card => {
+//     let randomPos = Math.floor(Math.random() * 12);
+//     card.style.order = randomPos;
+//   });
+// })();
+
+function resetCard() {
   
-    isMatch ? disableCards() : unflipCards();
-  }
+  this.classList.add('flip');
+  setTimeout(() => {
+ location.reload()
+  }, 900);
+
+
+
   
-  function disableCards() {
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
-  }
-  
-  function unflipCards() {
-    setTimeout(() => {
-      firstCard.classList.remove('flip');
-      secondCard.classList.remove('flip');
-    }, 800);
-  }
-  
-  cards.forEach(card => card.addEventListener('click', flipCard));
+}
+
+
+
+
+
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+reset.forEach(reset => reset.addEventListener('click', resetCard));
